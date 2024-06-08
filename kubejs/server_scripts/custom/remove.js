879888afa06c92fba05f6fcfbf12159243649a8a
JasonQ1123 <nmuqindexiang@163.com>
2024-06-08 14:24:43 +0800

    chocolate_cake

--- kubejs/server_scripts/custom/remove.js
+++ kubejs/server_scripts/custom/remove.js
@@ -1,11 +1,14 @@
 ServerEvents.recipes(e => {
   remove_recipes_mod(e, [
-
+    "createaddition"
   ]);
   remove_recipes_output(e, [
 
   ]);
   remove_recipes_id(e, [
-    
+    "enderio:cake",
+    "farmersdelight:cake_from_milk_bottle",
+    "minecraft:cake",
+    "create:crafting/curiosities/cake"
   ]);
 });
