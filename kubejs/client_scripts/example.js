19a49323c58dcb7933a66545770987b79071c118
JasonQ1123 <nmuqindexiang@163.com>
2024-06-08 10:53:18 +0800

    汉化

--- kubejs/client_scripts/example.js
+++ kubejs/client_scripts/example.js
@@ -1,6 +1,5 @@
-// priority: 0
-
-// Visit the wiki for more info - https://kubejs.com/
-
-console.info('Hello, World! (Loaded client scripts)')
-
+JEIEvents.hideItems(e=>{
+    e.hide([
+        
+    ])
+})
