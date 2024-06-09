3943f35a67eaa08276d54e831ffe8bf5ace7e884
LuJiChi <q3137647513@qq.com>
2024-06-09 19:48:51 +0800

    LuJiChi

--- kubejs/server_scripts/TFMG/recipes.js
+++ kubejs/server_scripts/TFMG/recipes.js
@@ -40,8 +40,7 @@ ServerEvents.recipes(e => {
         .heated()
         .id("craeteindustry:pressing/coal_sheet")
     // 碳棍合成
-    e.custom(
-        {
+    e.custom({
             "type": "createaddition:rolling",
             "input": {
                 "tag": "forge:plates/carbon"
@@ -50,8 +49,7 @@ ServerEvents.recipes(e => {
                 "item": "dimensionalrelics:carbon_rod",
                 "count": 2
             }
-        }
-    )
+        })
     // 铝板合成
     e.recipes.create.pressing("tfmg:aluminum_sheet", "tfmg:aluminum_ingot")
         .id("tfmg:pressing/aluminum_sheet")
