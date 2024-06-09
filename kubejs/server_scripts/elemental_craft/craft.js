019fb49bfaa43f7c3c8a3b8ca5bd3315148b43c4
lumine <463305152@qq.com>
2024-06-09 15:09:11 +0800

    elemental craft

---
+++ kubejs/server_scripts/elemental_craft/craft.js
@@ -0,0 +1,57 @@
+ServerEvents.recipes(e => {
+    //元素提取器
+    e.remove({output:'elementalcraft:extractor'})    
+    e.shaped(Item.of('elementalcraft:extractor',1), [
+        ' A ',
+        ' B ',
+        'BBB'
+    ],
+    {
+        A: 'minecraft:iron_ingot',
+        B: 'elementalcraft:contained_crystal'
+    })
+    //元素容器
+    e.remove({output:'elementalcraft:small_container'})    
+    e.shaped(Item.of('elementalcraft:small_container',1), [
+        ' A ',
+        'ABA',
+        ' A '
+    ],
+    {
+        A: 'elementalcraft:elementpipe_impaired',
+        B: 'create:fluid_tank'
+    })
+    //弱化元素导管
+    e.remove({output:'elementalcraft:elementpipe_impaired'})    
+    e.shaped(Item.of('elementalcraft:elementpipe_impaired',4), [
+        'BAB',
+        'ABA',
+        'BAB'
+    ],
+    {
+        A: 'elementalcraft:contained_crystal',
+        B: 'minecraft:iron_ingot'
+    })
+    //元素导管
+    e.remove({output:'elementalcraft:elementpipe'})    
+    e.shaped(Item.of('elementalcraft:elementpipe',4), [
+        'BAB',
+        'ABA',
+        'BAB'
+    ],
+    {
+        A: 'elementalcraft:elementpipe_impaired',
+        B: 'elementalcraft:drenched_iron_ingot'
+    })
+    //进阶元素导管
+    e.remove({output:'elementalcraft:elementpipe_improved'})    
+    e.shaped(Item.of('elementalcraft:elementpipe_improved',4), [
+        'BAB',
+        'ABA',
+        'BAB'
+    ],
+    {
+        A: 'elementalcraft:elementpipe',
+        B: 'elementalcraft:swift_alloy_ingot'
+    })
+})
