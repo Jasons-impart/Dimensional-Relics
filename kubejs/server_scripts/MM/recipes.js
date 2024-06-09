f19b86fc392bbf40a52aa7d8c15fe446e07a4a9e
JasonQ1123 <nmuqindexiang@163.com>
2024-06-09 17:58:55 +0800

    mm

--- kubejs/server_scripts/MM/recipes.js
+++ kubejs/server_scripts/MM/recipes.js
@@ -1,22 +1,4 @@
 MMEvents.createProcesses(e => {
-    e.create("mm:test_recipe")
-        .structureId("mm:test_structure")
-        .ticks(1000)
-        .input({
-            type: "mm:input/consume",
-            ingredient: {
-                type: "mm:item",
-                item: "minecraft:glass",
-                count: 10
-            }
-        })
-        .output({
-            type: "mm:output/simple",
-            ingredient: {
-                type: "mm:energy",
-                amount: 1000
-            }
-        })
     e.create("mm:polyethylene")
         .structureId("mm:polymerization_reactor")
         .ticks(1000)
--- kubejs/server_scripts/MM/structures.js
+++ kubejs/server_scripts/MM/structures.js
@@ -1,22 +1,6 @@
 MMEvents.createStructures(e => {
-    e.create("mm:test_structure")
-        .controllerId("mm:test_controller")
-        .name("Test Machine")
-        .layout(a => {
-            a.layer([
-                "BCA"
-            ]).layer([
-                "DDD"
-            ]).key("A", {
-                block: "mm:test_input"
-            }).key("B", {
-                block: "mm:test_output"
-            }).key("D", {
-                block: "minecraft:glass"
-            })
-        })
     e.create("mm:polymerization_reactor")
-        .controllerId("mm:test_controller")
+        .controllerId("mm:steel_controller")
         .name("Polymerization Reactor")
         .layout(a => {
             a.layer([
@@ -34,15 +18,15 @@ MMEvents.createStructures(e => {
             ]).key("A", {
                 block: "tfmg:heavy_machinery_casing"
             }).key("B", {
-                block: "mm:test_energy_port_input"
+                block: "mm:steel_energy_port_input"
             }).key("D", {
-                block: "mm:test_item_port_output"
+                block: "mm:steel_item_port_output"
             }).key("E", {
-                block: "mm:test_fluid_port_input"
+                block: "mm:steel_fluid_port_input"
             }).key("F", {
                 block: "create:mechanical_mixer"
             }).key("G", {
-                block: "mm:test_create_port_input"
+                block: "mm:steel_create_port_input"
             })
         })
 })
