4c255432f98b4e46d1e737fc2e7231c0972fe00c
JasonQ1123 <nmuqindexiang@163.com>
2024-06-09 17:04:44 +0800

    mm first worked

--- kubejs/server_scripts/MM/recipes.js
+++ kubejs/server_scripts/MM/recipes.js
@@ -17,4 +17,46 @@ MMEvents.createProcesses(e => {
                 amount: 1000
             }
         })
+    e.create("mm:polyethylene")
+        .structureId("mm:polymerization_reactor")
+        .ticks(1000)
+        .input({
+            type: "mm:input/consume",
+            ingredient: {
+                type: "mm:fluid",
+                fluid: "tfmg:liquid_plastic",
+                amount: 500
+            }
+        })
+        .input({
+            type: "mm:input/consume",
+            ingredient: {
+                type: "mm:energy",
+                amount: 1000
+            }
+        })
+        .input({
+            type: "mm:input/consume",
+            ingredient: {
+                type: "mm:create/kinetic",
+                speed: 128
+            }
+        })
+        .output({
+            type: "mm:output/simple",
+            ingredient: {
+                type: "mm:item",
+                item: "tfmg:plastic_sheet",
+                count: 1
+            }
+        })
+        .output({
+            type: "mm:output/simple",
+            chance: 0.5,
+            ingredient: {
+                type: "mm:item",
+                item: "tfmg:plastic_sheet",
+                count: 1
+            }
+        })
 })
