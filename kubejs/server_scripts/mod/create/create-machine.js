aadbca0da01d2485049c99a6d9d20e3e935ecf77
LuJiChi <q3137647513@qq.com>
2024-06-10 23:00:44 +0800

    旋转力压缩机与压缩空气引擎的配方

--- kubejs/server_scripts/mod/create/create-machine.js
+++ kubejs/server_scripts/mod/create/create-machine.js
@@ -6,8 +6,10 @@ ServerEvents.recipes(function (event) {
     event.remove({ id: "create:crafting/materials/andesite_alloy_from_zinc" });
     event.remove({ id: "create:mixing/andesite_alloy_from_zinc" });
     event.remove({ id: "create:mixing/andesite_alloy" });
+    event.remove({ id: "compressedcreativity:crafting/rotational_compressor" });
+    event.remove({ id: "compressedcreativity:crafting/compressed_air_engine" });
     // 强化石
-    event.recipes.create.compacting('pneumaticcraft:reinforced_stone', ['tfmg:steel_ingot', 'minecraft:stone']).heated();
+    event.recipes.create.compacting('pneumaticcraft:reinforced_stone', ['mekanism:ingot_steel', 'minecraft:stone']).heated();
     //安山合金混合液1
     event.recipes.create.mixing(['dimensionalrelics:andesite_alloy'], [Fluid.lava(400), '5x minecraft:andesite', '2x minecraft:iron_ingot', 'minecraft:kelp']);
     //安山合金混合液2    
@@ -16,4 +18,27 @@ ServerEvents.recipes(function (event) {
     event.recipes.create.compacting('create:andesite_alloy_block', [Fluid.of('dimensionalrelics:andesite_alloy')]);
     //门瑞欧树脂
     event.recipes.create.compacting('integrateddynamics:menril_resin', 'integrateddynamics:menril_log');
+    event.recipes.create.mechanical_crafting('compressedcreativity:rotational_compressor', [
+        "ABA",
+        "CDC",
+        "EFE"
+    ], {
+        A: 'pneumaticcraft:reinforced_bricks',
+        B: 'create:propeller',
+        C: 'pneumaticcraft:pressure_tube',
+        D: 'create:brass_casing',
+        E: 'create:cogwheel',
+        F: 'create:shaft'
+    });
+    event.recipes.create.mechanical_crafting('compressedcreativity:compressed_air_engine', [
+        " AB",
+        "CDE",
+        " CB"
+    ], {
+        A: 'pneumaticcraft:pressure_tube',
+        B: 'create:brass_ingot',
+        C: 'create:copper_sheet',
+        D: 'compressedcreativity:engine_rotor',
+        E: 'mekanism:ingot_steel'
+    });
 });
