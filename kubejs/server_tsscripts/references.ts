25248f7de1a36caf2570e11c4a0bda57e7fad9be
CPearl0 <1269596929@qq.com>
2024-06-10 20:14:29 +0800

    ref

--- kubejs/server_tsscripts/references.ts
+++ kubejs/server_tsscripts/references.ts
@@ -4,6 +4,6 @@
 
 /// <reference path="../probe/generated/globals.d.ts" />
 /// <reference path="../probe/generated/events.d.ts" />
-/// <reference path="../probe/generated/internals/internal_32.d.ts" />
 /// <reference path="../probe/generated/internals/internal_12.d.ts" />
 /// <reference path="../probe/generated/internals/internal_26.d.ts" />
+/// <reference path="../probe/generated/internals/internal_32.d.ts" />
