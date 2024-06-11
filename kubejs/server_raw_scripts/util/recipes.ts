3f9ff2d515be2a0af3eb12183ab3ceace61e5036
CPearl0 <1269596929@qq.com>
2024-06-11 23:30:50 +0800

    代码警察

--- kubejs/server_raw_scripts/util/recipes.ts
+++ kubejs/server_raw_scripts/util/recipes.ts
@@ -1,26 +1,26 @@
 // priority: 100
 
-function remove_recipes_output(event: Internal.RecipesEventJS, items: string[]): void {
+function remove_recipes_output(event: Internal.RecipesEventJS, items: string[]) {
     event.remove({ output: items });
 }
 
-function remove_recipes_input(event: Internal.RecipesEventJS, items: string[]): void {
+function remove_recipes_input(event: Internal.RecipesEventJS, items: string[]) {
     event.remove({ input: items });
 }
 
-function remove_recipes_id(event: Internal.RecipesEventJS, ids: string[]): void {
+function remove_recipes_id(event: Internal.RecipesEventJS, ids: string[]) {
     ids.forEach(id => {
         event.remove({ id: id });
     });
 }
 
-function remove_recipes_type(event: Internal.RecipesEventJS, types: string[]): void {
+function remove_recipes_type(event: Internal.RecipesEventJS, types: string[]) {
     types.forEach(type => {
         event.remove({ type: type });
     });
 }
 
-function remove_recipes_mod(event: Internal.RecipesEventJS, mods: string[]): void {
+function remove_recipes_mod(event: Internal.RecipesEventJS, mods: string[]) {
     mods.forEach(mod => {
         event.remove({ mod: mod });
     });
