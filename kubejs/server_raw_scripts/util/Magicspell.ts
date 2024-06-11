06b651400ea2b8085cbb3f2e5d832c0d537246c0
mixiaobaozi <2206124398@qq.com>
2024-06-11 18:47:20 +0800

    巧工轮子

---
+++ kubejs/server_raw_scripts/util/Magicspell.ts
@@ -0,0 +1,42 @@
+// priority: 100
+
+//巧工织魔祭坛
+function manaweaving_recipe(event: Internal.RecipesEventJS, tier:number, output:string[], quantity:number, items:string[], patterns:string[]) {
+    event.custom({
+        type: "mna:manaweaving-recipe",
+        tier: tier, // 等阶
+        output: output, // 输出物品
+        quantity: quantity, // 输出个数
+        items: items, // 输入物品
+        patterns: patterns // 织魔图案
+    });
+}
+//巧工嬗变
+function manatransmutation(event: Internal.RecipesEventJS, targetBlock: string,replaceBlock: string){
+    event.custom({
+        type: "mna:transmutation",
+        targetBlock: targetBlock,
+        replaceBlock: replaceBlock
+    })
+}
+ //巧工符文铁砧
+ function manaruneforging(event: Internal.RecipesEventJS, tier: number, material: string, hits:number, pattern:string[], output:string){
+    event.custom({
+        "type": "mna:runeforging",
+	    "tier": tier,//等阶
+	    "material": material,//材料
+	    "hits": hits,//次数
+	    "pattern": pattern,//符文模板
+	    "output": output//输出
+    })
+ }
+ //巧工符文锻炉
+ function manacrushing(event: Internal.RecipesEventJS, tier: number, input: string, output:string, output_quantity: number){
+    event.custom({
+        "type": "mna:crushing",
+	    "tier": tier,
+	    "input": input,
+	    "output": output,
+	    "output_quantity": output_quantity
+    })
+ }
