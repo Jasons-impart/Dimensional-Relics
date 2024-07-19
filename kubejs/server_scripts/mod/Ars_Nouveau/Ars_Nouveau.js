7a271b0598ffb66c6fb7cfe1436d34be5b3ddad6
LuJiChi <q3137647513@qq.com>
2024-07-19 16:14:20 +0800

    部分新生魔艺精华修改，没点子了

--- kubejs/server_scripts/mod/Ars_Nouveau/Ars_Nouveau.js
+++ kubejs/server_scripts/mod/Ars_Nouveau/Ars_Nouveau.js
@@ -1,5 +1,8 @@
 ServerEvents.recipes(function (e) {
     e.remove({ output: 'ars_nouveau:imbuement_chamber' });
+    e.remove({ id: 'ars_nouveau:imbuement_manipulation_essence' });
+    e.remove({ id: 'ars_nouveau:imbuement_fire_essence' });
+    e.remove({ id: 'ars_nouveau:scribes_table' });
     //灌注室
     e.shaped(Item.of('ars_nouveau:imbuement_chamber'), [
         "ABA",
@@ -10,5 +13,63 @@ ServerEvents.recipes(function (e) {
         B: "elementalcraft:swift_alloy_ingot",
         C: "ars_nouveau:source_gem",
         D: "botania:terrasteel_ingot"
+    });
+    //操纵之精华
+    e.custom({
+        "type": "ars_nouveau:imbuement",
+        "count": 1,
+        "input": {
+            "tag": "forge:gems/source"
+        },
+        "output": "ars_nouveau:manipulation_essence",
+        "pedestalItems": [
+            { "item": {
+                    "item": "minecraft:redstone"
+                } },
+            { "item": {
+                    "item": "minecraft:piston"
+                } },
+            { "item": {
+                    "item": "minecraft:clock"
+                } }
+        ],
+        "source": 2000
+    });
+    //火之精华
+    e.custom({
+        "type": "ars_nouveau:imbuement",
+        "count": 1,
+        "input": {
+            "tag": "forge:gems/source"
+        },
+        "output": "ars_nouveau:fire_essence",
+        "pedestalItems": [
+            {
+                "item": {
+                    "item": "minecraft:flint_and_steel"
+                }
+            },
+            {
+                "item": {
+                    "item": "minecraft:campfire"
+                }
+            },
+            {
+                "item": {
+                    "item": "minecraft:gunpowder"
+                }
+            }
+        ],
+        "source": 2000
+    });
+    //抄写桌
+    e.shaped('ars_nouveau:scribes_table', [
+        "AAA",
+        "B B",
+        "C C"
+    ], {
+        A: 'ars_nouveau:archwood_slab',
+        B: 'minecraft:gold_ingot',
+        C: '#forge:logs/archwood'
     });
 });
